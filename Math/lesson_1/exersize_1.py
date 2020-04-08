1f0580dbbe975458912b95518b5bf0eb6fcd0afa
Tixon <tixondamage@gmail.com>
2020-04-08 15:47:46 +0300

    test

--- Math/lesson_1/exersize_1.py
+++ Math/lesson_1/exersize_1.py
@@ -1,13 +1,2 @@
 import numpy as np
 
-matrix = np.array([[0, 1, 2], [1, 2, 3], [2, 3, 4]])
-
-def create_matrix(x, y, variable):
-
-
-    for i in range(y):
-        for j in range(x):
-            matrix[i, j] = variable
-    return matrix
-
-print(create_matrix(3, 4, 12))
